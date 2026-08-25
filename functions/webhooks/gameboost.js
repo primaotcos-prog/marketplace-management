3b51fc59e8a0eb40b85bc2e3eb5888b1c31444a4
primaotcos-prog <primaotcos@gmail.com>
2026-08-26 00:45:15 +0700

    Route GameBoost webhook through shared handler

--- functions/webhooks/gameboost.js
+++ functions/webhooks/gameboost.js
@@ -1,3 +1,1 @@
-// GameBoost webhook handler is deployed through _worker.js.
-// Keep this file as the shared Pages-style handler implementation.
 export { onRequestGet, onRequestPost } from "./gameboost-handler.js";
