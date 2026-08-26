95bbad65c3a21389a526363aea0273cc52f05bb2
primaotcos-prog <primaotcos@gmail.com>
2026-08-26 10:19:52 +0700

    Add webhook order test endpoint

---
+++ functions/webhooks/order-dashboard-test.js
@@ -0,0 +1,1 @@
+export async function orderDashboardTest(){return new Response(JSON.stringify({ok:true,test:true,message:'Order dashboard test endpoint ready'}),{headers:{'content-type':'application/json'}})}
