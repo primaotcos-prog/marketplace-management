2fdba260ccaf4b899e7c3951d439e5066df850d7
primaotcos-prog <primaotcos@gmail.com>
2026-08-28 01:34:57 +0700

    reconnect pricing exports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-// placeholder
+import { supabase } from "./supabase.js";
+import { loadCompetitivePreview, savePricingRule, getPricingHistory, renderCompetitorPreview } from "./pricing.js";
+export { loadCompetitivePreview, savePricingRule, getPricingHistory, renderCompetitorPreview };
